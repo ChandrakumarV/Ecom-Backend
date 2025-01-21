5da1399c5ca481b78552fa2b486673a5fdde9b3a
ChandrakumarV <chandrakumar252004@gmail.com>
2025-01-22 02:05:32 +0530

    schema fix

--- dist/src/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    env: process.env.NODE_ENV || 'development',
-    port: parseInt(process.env.PORT || '3000')
-};
-exports.default = config;
